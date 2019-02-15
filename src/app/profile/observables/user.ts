ac472e560d884d7c79201325814d615f1389eb4a
AdityaDKulkarni <kulkarniaditya1997@gmail.com>
2019-02-15 15:29:30 +0530

    Login and registration form

---
+++ src/app/profile/observables/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  public id: number;
+  public username: string;
+  public password: string;
+  public email: string;
+  public firstName: string;
+  public lastName: string;
+}
